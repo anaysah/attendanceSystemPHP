db742e2b104e3b226d3849f7be1756e5648681eb
anaysah <ajaysahu0606@gmail.com>
2023-03-25 09:58:19 +0530

    white theme and changeTheme btn

--- js/headers.js
+++ js/headers.js
@@ -5,3 +5,15 @@ function closeMessageBox(event){
         div.remove();
     }, 500);
 }
+
+function changeTheme(event){
+    document.body.classList.toggle("white-theme");
+    const label = document.getElementById("theme-label");
+    const icon = document.querySelector(".changeTheme-icon");
+    icon.classList.toggle("fa-sun");
+    if (document.body.classList.contains("white-theme")) {
+        label.textContent = "Light";
+    } else {
+        label.textContent = "Dark";
+    }
+}
--- templates/header.php
+++ templates/header.php
@@ -12,6 +12,8 @@ session_start();
     <title>Document</title>
     <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css'>
     <link rel='stylesheet' href='https://unicons.iconscout.com/release/v2.1.9/css/unicons.css'>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/84ae347da1.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="styles/headers.css">
 </head>
 
@@ -19,6 +21,10 @@ session_start();
     <main>
         <header class="bgImage">
             <menu>
+                <span onclick="changeTheme(event)" class="changeTheme-btn">
+                    <span id="theme-label">Dark</span>
+                    <i class="fa-solid fa-star fa-lg changeTheme-icon" ></i>
+                </span>
                 <?php
                 if (isset($_SESSION["id"])) {
                     echo "<span><a href='home.php'>Home</a></span>";
@@ -33,18 +39,17 @@ session_start();
 
             <div id="messageBox">
                 <?php
-                if(isset($_GET["error"])){
+                if (isset($_GET["error"])) {
                     echo "
                 <div id='messageCard'>
-                    <span class='message'>".$_GET["error"]."</span>
+                    <span class='message'>" . $_GET["error"] . "</span>
                     <span class='cross-icon' onclick='closeMessageBox(event)'>&#x2716;</span>
                 </div>
                 ";
-                unset($_GET['id']);
+                    unset($_GET['id']);
                 }
                 ?>
             </div>
         </header>
-
-        <script src="js/headers.js"></script>
         
+        <script src="js/headers.js"></script>
