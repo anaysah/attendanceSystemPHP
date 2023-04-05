f8552ac5c18ccf962c3efc3630061fb39b0461e8
anaysah <ajaysahu0606@gmail.com>
2023-04-05 09:46:22 +0530

    attendance page student| present or absent or onleave

--- templates/sidemenu.inc.php
+++ templates/sidemenu.inc.php
@@ -17,7 +17,7 @@
 
 
 <script>
-    var links = { "side-menu-top": "../<?=$_SESSION["userType"]?>/attendance.php","peoples":"../<?=$_SESSION["userType"]?>/peoples.php","attendance":"../teacher/allattendance.php"}
+    var links = { "side-menu-top": "../<?=$_SESSION["userType"]?>/attendance.php","peoples":"../<?=$_SESSION["userType"]?>/peoples.php","attendance":"../<?=$_SESSION["userType"]?>/allattendance.php"}
     for (let id in links) {
         const myDiv = document.getElementById(id);
         myDiv.addEventListener("click", function () {
