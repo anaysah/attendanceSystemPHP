c926cbb52dad371061227a46d77b2e9c03b0861e
anaysah <ajaysahu0606@gmail.com>
2023-04-22 12:31:47 +0530

    changed auth UI

--- auth.php
+++ auth.php
@@ -2,96 +2,104 @@
 
 <link rel="stylesheet" href="styles/auth.css">
 <section id="auth-section">
-    <div class="auth-box">
-        <div class="auth-nav">
-            <span class="auth-nav-Btn" id="auth-nav-loginBtn">Log In</span>
-            <span class="auth-nav-Btn" id="auth-nav-signupBtn">Sign Up</span>
+<div class="wrapper bgImage">
+            <div class="title-text">
+                <div class="title login">Login Form</div>
+                <div class="title signup">Signup Form</div>
+            </div>
+            <div class="form-container">
+                <div class="slide-controls">
+                    <input type="radio" name="slide" id="login" checked>
+                    <input type="radio" name="slide" id="signup">
+                    <label for="login" class="slide login">Login</label>
+                    <label for="signup" class="slide signup">Signup</label>
+                    <div class="slider-tab"></div>
+                </div>
+                <div class="user-type-box">
+                    <input type="radio" name="user-type-slide" id="teacher" checked>
+                    <input type="radio" name="user-type-slide" id="student">
+                    <label for="teacher" class="slide teacher">Teacher</label>
+                    <label for="student" class="slide student">Student</label>
+                    <div class="slider-tab"></div>
+                </div>
+                
+                <div class="form-inner">
+                    
+                    <form action="#" class="login">
+                        <div class="field">
+                            <input type="email" name="login-email" class="form-style" placeholder="Your Email"
+                                id="logemail" autocomplete="off">
+                            <i class="input-icon uil uil-at"></i>
+                        </div>
+                        
+                        <div class="field">
+                            <input type="password" name="login-pass" class="form-style" placeholder="Your Password"
+                                id="logpass" autocomplete="off">
+                            <i class="input-icon uil uil-lock-alt"></i>
+                        </div>
+                        <div class="pass-link"><a href="#">Forgot password?</a></div>
+                        <div class="field field-btn">
+                            <div class="btn-layer"></div>
+                            <input type="submit" value="Login">
+                        </div>
+                        <div class="signup-link">Not a member? <a href="">Signup now</a></div>
+                    </form>
+                    <form action="#" class="signup">
+                        
+                        <div class="field">
+                            <input type="text" name="signup-name" class="form-style" placeholder="Your Full Name" id="signname"
+                                autocomplete="off">
+                            <i class="input-icon uil uil-user"></i>
+                        </div>
+        
+                        <div class="field">
+                            <input type="email" name="signup-email" class="form-style" placeholder="Your Email" id="signmail"
+                                autocomplete="off">
+                            <i class="input-icon uil uil-at"></i>
+                        </div>
+        
+                        <div class="field">
+                            <input type="password" name="signup-pass" class="form-style" placeholder="Your Password"
+                                id="signpass" autocomplete="off">
+                            <i class="input-icon uil uil-lock-alt"></i>
+                        </div>
+        
+                        <div class="field">
+                            <input type="password" name="signup-rpass" class="form-style" placeholder="Repeat Password"
+                                id="signrpass" autocomplete="off">
+                            <i class="input-icon uil uil-lock-alt"></i>
+                        </div>
+                        <div class="field field-btn">
+                            <div class="btn-layer"></div>
+                            <input type="submit" value="Signup">
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
-
-
-        <div class="auth-card bgImage" id="loginBox">
-
-            <form action="includes/login.inc.php" method="post">
-                <div class="user_type_box mb-2">
-                    <label class="user_type_lable selectedRadio" onclick="selectUserType(event)">
-                        Teacher
-                        <input type="radio" name="user-type" value="teacher" class="hiddenRadio" checked>
-                    </label>
-                    <label class="user_type_lable" onclick="selectUserType(event)">
-                        Student
-                        <input type="radio" name="user-type" value="student" class="hiddenRadio">
-                    </label>
-                </div>
-                <div class="form-group">
-                    <input type="email" name="login-email" class="form-style" placeholder="Your Email" id="logemail"
-                        autocomplete="off">
-                    <i class="input-icon uil uil-at"></i>
-                </div>
-
-                <div class="form-group mt-2">
-                    <input type="password" name="login-pass" class="form-style" placeholder="Your Password" id="logpass"
-                        autocomplete="off">
-                    <i class="input-icon uil uil-lock-alt"></i>
-                </div>
-
-                <input type="submit" value="submit" name="submit" class="btn mt-4">
-                <a href="#0" class="link">Forgot your password?</a>
-            </form>
-
-        </div>
-
-
-        <div class="auth-card" id="signupBox">
-
-            <form action="includes/signup.inc.php" method="post">
-                <div class="user_type_box mb-2">
-                    <label class="user_type_lable selectedRadio" onclick="selectUserType(event)">
-                        Teacher
-                        <input type="radio" name="user-type" value="teacher" class="hiddenRadio" checked>
-                    </label>
-                    <label class="user_type_lable" onclick="selectUserType(event)">
-                        Student
-                        <input type="radio" name="user-type" value="student" class="hiddenRadio">
-                    </label>
-                </div>
-
-                <div class="form-group">
-                    <input type="text" name="signup-name" class="form-style" placeholder="Your Full Name" id="signname"
-                        autocomplete="off">
-                    <i class="input-icon uil uil-user"></i>
-                </div>
-
-                <div class="form-group mt-2">
-                    <input type="email" name="signup-email" class="form-style" placeholder="Your Email" id="signmail"
-                        autocomplete="off">
-                    <i class="input-icon uil uil-at"></i>
-                </div>
-
-                <div class="form-group mt-2">
-                    <input type="password" name="signup-pass" class="form-style" placeholder="Your Password"
-                        id="signpass" autocomplete="off">
-                    <i class="input-icon uil uil-lock-alt"></i>
-                </div>
-
-                <div class="form-group mt-2">
-                    <input type="password" name="signup-rpass" class="form-style" placeholder="Repeat Password"
-                        id="signrpass" autocomplete="off">
-                    <i class="input-icon uil uil-lock-alt"></i>
-                </div>
-
-                <input type="submit" name="submit" value="submit" class="btn mt-4">
-            </form>
-
-        </div>
-
-    </div>
-
-    <div id="image">
-        <img src="https://i.ibb.co/h87hcqM/image.png" alt="image" border="0">
-    </div>
 
 
 </section>
+<script>
+        const loginText = document.querySelector(".title-text .login");
+        const loginForm = document.querySelector("form.login");
+        const loginBtn = document.querySelector("label.login");
+        const signupBtn = document.querySelector("label.signup");
+        const signupLink = document.querySelector("form .signup-link a");
+        signupBtn.onclick = (() => {
+            loginForm.style.marginLeft = "-50%";
+            loginText.style.marginLeft = "-50%";
+        });
+        loginBtn.onclick = (() => {
+            loginForm.style.marginLeft = "0%";
+            loginText.style.marginLeft = "0%";
+        });
+        signupLink.onclick = (() => {
+            signupBtn.click();
+            return false;
+        });
+
+    </script>
 
 
 <?php include_once('templates/footer.php') ?>
